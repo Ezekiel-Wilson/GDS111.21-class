8bb10cd22fb1225fdbb02c9f69b49374a425afc2
Veldman-Wilson <ehveldmanwilson@labs.neit.edu>
2021-10-26 17:36:21 -0400

    git

---
+++ 03_Veldman-Wilson/week3.js
@@ -0,0 +1,37 @@
+var store1="A Map"
+var store2="Echo"
+var store3="NOON"
+var store4="The letter “R.”"
+var store5="All the people were married."
+var store6="Bookkeeper"
+var store7="She took a picture of him and developed it in her darkroom."
+
+function answer1() {
+
+
+    document.getElementById("question1").innerHTML = store1;
+}
+function answer2() {
+
+    document.getElementById("question2").innerHTML = store2;
+}
+function answer3(){
+
+    document.getElementById("question3").innerHTML = store3;
+}
+function answer4(){
+
+    document.getElementById("question4").innerHTML = store4;
+}
+function answer5(){
+
+    document.getElementById("question5").innerHTML = store5;
+}
+function answer6(){
+
+    document.getElementById("question6").innerHTML = store6;
+}
+function answer7(){
+
+    document.getElementById("question7").innerHTML = store7;
+}
