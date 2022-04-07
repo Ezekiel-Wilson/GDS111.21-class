dcc0bafbede4f3086bdd050a0919272537fe3b56
Veldman-Wilson <ehveldmanwilson@email.neit.edu>
2022-04-07 15:14:38 -0400

    week2

--- week2/js/game.js
+++ week2/js/game.js
@@ -3,6 +3,7 @@ var context;
 var timer;
 var interval = 1000/60;
 var player1;
+var ball;
 
 
 
@@ -16,8 +17,13 @@ var player1;
 	
 	var player1 = new GameObject();
 	player1.x = 100;
-	player1.width = 20;
-	player1.height = 60;
+	player1.width = 40;
+	player1.height = 120;
+	ball = new GameObject();
+	
+	//------Declare the Player's speed on the x and y axis------
+	ball.vx =15;
+	ball.vy = 15;	
 	
 	//Set the Animation Timer
 	timer = setInterval(animate, interval);
@@ -31,8 +37,62 @@ function animate()
 
 	
 	//Update the Screen
-	player1.drawRect();
 	
+	
+	if(s)
+	{
+		player1.y+=10;
+	}
+	if(w)
+	{
+		player1.y-=10;
+	}
+	
+	if(player1.y > canvas.height - player1.width)
+	{
+		player1.y = canvas.height - player1.width
+		
+		player1.color="yellow"
+	}
+	if(player1.y < 0 + player1.height/2)
+	{
+	player1.y = 0 + player1.height/2
+		player1.color="lime"}
+		
+		
+		//----Movement Using the Player's move() function----
+		ball.move();
+		//---------------------------------------------------
+		
+		//--------------Bounce of Right----------------------
+		if(ball.x > canvas.width - ball.width/2)
+		{
+			ball.x = canvas.width - ball.width/2
+			ball.vx = -ball.vx;
+			ball.color="orange"
+		}
+		if(ball.x < 0 + ball.height/2)
+		{	
+		ball.vx = -ball.vx;
+		ball.color="pink"
+		}
+		if(ball.y > canvas.height - ball.width/2)
+		{
+			ball.y = canvas.height - ball.width/2
+			ball.vy = -ball.vy;
+		ball.color="yellow"
+		}
+		if(ball.y < 0 + ball.height/2)
+		{ball.vy = -ball.vy
+		ball.color="lime"}
+			
+			
+			
+			
+			//---------------------------------------------------
+			
+			ball.drawCircle();
+			player1.drawRect();
+		}
 
 
-}
