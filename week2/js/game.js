570751b5f3c004d82a6b7120da61a1242e4204aa
Veldman-Wilson <ehveldmanwilson@email.neit.edu>
2022-04-25 14:49:05 -0400

    gds121

--- week2/js/game.js
+++ week2/js/game.js
@@ -8,8 +8,8 @@ var prevY;
 var prevX;
 var playerY;
 var player2;
-var p1wins;
-var p2wins;
+var p1wins=0;
+var p2wins=0;
 
 
 
@@ -59,17 +59,17 @@ function animate()
 	//top
 	if(ball.hitTestObject(player1))
 	{
-		ball.x = player1.x + player1.width/1 + ball.width/1
+		ball.x = player1.x + player1.width/2 + ball.width/2
 		ball.vx= -ball.vx;
 		if(ball.y < player1.y - player2.height/6)
 		{
 		
-			ball.vy=+6;
+			ball.vy=-6;
 		}
 		if(ball.y > player1.y)
 		{
-			ball.vx= +6
-			ball.vy = 6
+			ball.vx= 6;
+			ball.vy = 6;
 			
 		}
 	}
@@ -87,10 +87,10 @@ function animate()
 		
 			ball.vy=-6;
 		}
-		if(ball.y > player2.y)
+		if(ball.y > player2.y + player2.height/6)
 		{
-			ball.vx= -6
-			ball.vy = 6
+			ball.vx= -6;
+			ball.vy = 6;
 			
 		}
 	}
@@ -151,13 +151,13 @@ function animate()
 		{	
 		ball.x = canvas.width/2
 		ball.color="orange"
-		p1wins = +1
+		p1wins = p1wins+1
 		}
 		if(ball.x < 0 + ball.height/2)
 		{	
 		ball.vx = -ball.vx;
 		ball.color="pink"
-		p2wins = +2
+		p2wins = p2wins+1
 		}
 		if(ball.y > canvas.height - ball.width/2)
 		{
@@ -175,10 +175,20 @@ function animate()
 			//---------------------------------------------------
 			context.font = "20px Arial";
 			context.fillText("Player One || Player Two" , 400, 25);
-			context.fillText("Score",p1wins,p2wins, 400,35);
+			context.fillText(p1wins + "|"+ p2wins, 497.5,59);
 			ball.drawCircle();
 			player1.drawRect();
 			player2.drawRect()
+			context.save();
+		
+			context.beginPath();
+			context.moveTo(512,0);
+			context.lineTo(512,800);
+			context.closePath();
+			context.lineWidth = 2;
+			context.stroke();
+			context.restore();
+
 		}
 
 
