802652f5468ca38a7cc630d1caa7e064be5cf1c7
Veldman-Wilson <ehveldmanwilson@email.neit.edu>
2022-05-09 12:39:58 -0400

    gds127

--- 4.7/js/basic_platformer.js
+++ 4.7/js/basic_platformer.js
@@ -10,7 +10,7 @@ var player;
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");	
 
-	player = new GameObject({x:500, y:canvas.height/2-100});
+	player = new GameObject({x:300, y:canvas.height/2-100});
 
 	platform0 = new GameObject();
 		platform0.width = canvas.width-300;
@@ -19,10 +19,9 @@ var player;
 		
 	
 	platform1 = new GameObject();
-		platform1.x = platform1.width;
+		platform1.x = 500;
 		platform1.y = platform0.y- platform0.height/2 - platform1.height/2;
-		platform1.color = "#66ff33";
-		platform1.vx = 3;
+		platform1.color = "#ffff00";
 		
 	platform2 = new GameObject();
 		platform2.width = canvas.width-300;
@@ -33,7 +32,7 @@ var player;
 
 		
 	
-	goal = new GameObject({width:24, height:50, x:platform1.x, y:platform1.y+100, color:"#00ffff"});
+	goal = new GameObject({width:24, height:50, x:700, y:platform0.y-100, color:"#00ffff"});
 	
 
 	var fX = .85;
@@ -116,23 +115,43 @@ function animate()
 		player.x--;
 		player.vx = 0;
 	}
-	
-	//---------Objective: Save Me!---------------------------------------------------------------------------------------------------- 
-	//---------Run this program first.
-	//---------Change the following condition so that the player pushes the wall-------------------------------------------------
-
-	while(platform1.hitTestPoint(player.left() ))
+	while(platform1.hitTestPoint(player.top()) && player.vy <=0)
 	{
-		platform1.x--;
-				
-		
-		
-		
+		player.y++;
+		player.vy = 0;
+	}
+	while(platform1.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	while(platform1.hitTestPoint(player.left()) && player.vx <=0)
+	{
+		player.x++;
+		player.vx = 0;
+	}
+	while(platform1.hitTestPoint(player.right()) && player.vx >=0)
+	{
+		player.x--;
+		player.vx = 0;
+		platform1.x = canvas.width*2;
 	}
 	
 	
+	//---------Objective: Let Me Out!---------------------------------------------------------------------------------------------------- 
+	//---------Run this program first.
+	//---------Write a condition so that the player opens the yellow door to get the pearl-----------------------------------------
 
 	
+	
+
+
+
+	if(player.hitTestObject(goal))
+	{
+		goal.y = 10000;
+	}
 	
 
 
@@ -151,6 +170,6 @@ function animate()
 	
 	//Show hit points
 	player.drawDebug();
-	
+	goal.drawCircle();
 }
 
